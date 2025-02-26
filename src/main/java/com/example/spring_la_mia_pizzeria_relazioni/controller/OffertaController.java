5096ec30ccb809ae400db13a49c83b80d6483070
PaoloFabris98 <f98paolo@gmail.com>
2025-02-26 20:48:13 +0100

    feat:better

--- src/main/java/com/example/spring_la_mia_pizzeria_relazioni/controller/OffertaController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_relazioni/controller/OffertaController.java
@@ -1,24 +1,16 @@
 package com.example.spring_la_mia_pizzeria_relazioni.controller;
 
-import java.util.List;
-import java.util.Optional;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.validation.Valid;
-
-import com.example.spring_la_mia_pizzeria_relazioni.model.Pizza;
 import com.example.spring_la_mia_pizzeria_relazioni.model.SpecialOffers;
 import com.example.spring_la_mia_pizzeria_relazioni.service.PizzaService;
 import com.example.spring_la_mia_pizzeria_relazioni.service.SpecialOffersService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OffertaController {
@@ -31,7 +23,8 @@ public class OffertaController {
     public String offerta(@PathVariable Integer id, Model model) {
         SpecialOffers specialOffers = new SpecialOffers();
         specialOffers.setPizzeFixedDiscount(pizzaService.findById(id));
-        model.addAttribute("offerta", new SpecialOffers());
+        specialOffers.setDiscountStart(LocalDate.now());
+        model.addAttribute("offerta", specialOffers);
         return new String();
     }
 
--- src/main/java/com/example/spring_la_mia_pizzeria_relazioni/service/PizzaService.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_relazioni/service/PizzaService.java
@@ -1,7 +1,6 @@
 package com.example.spring_la_mia_pizzeria_relazioni.service;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
