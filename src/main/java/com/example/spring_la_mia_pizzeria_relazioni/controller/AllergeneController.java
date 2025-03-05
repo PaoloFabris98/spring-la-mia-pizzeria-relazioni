f78e87194876557e23f9c6c589c36ab1f60f2a88
PaoloFabris98 <f98paolo@gmail.com>
2025-03-05 10:55:19 +0100

    feat:better

--- src/main/java/com/example/spring_la_mia_pizzeria_relazioni/controller/AllergeneController.java
+++ src/main/java/com/example/spring_la_mia_pizzeria_relazioni/controller/AllergeneController.java
@@ -1,13 +1,15 @@
 package com.example.spring_la_mia_pizzeria_relazioni.controller;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 
 import com.example.spring_la_mia_pizzeria_relazioni.model.Allergene;
-import com.example.spring_la_mia_pizzeria_relazioni.model.Pizza;
 import com.example.spring_la_mia_pizzeria_relazioni.service.AllergeniService;
 
 import jakarta.validation.Valid;
@@ -18,8 +20,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.nio.file.Path;
 
 @Controller
 public class AllergeneController {
@@ -34,7 +39,8 @@ public class AllergeneController {
     }
 
     @PostMapping("/creaAllergene")
-    public String addAllergene(@Valid @ModelAttribute("allergene") Allergene formAllergene, BindingResult bindingResult,
+    public String addAllergene(@Valid @ModelAttribute("allergene") Allergene formAllergene,
+            BindingResult bindingResult,
             RedirectAttributes redirectAttributes) {
 
         if (bindingResult.hasErrors()) {
